76e5b599828eb916f81618b3e716ffc43022426c
Tunde Osborne <osbornetunde@gmail.com>
2018-06-23 23:13:57 +0100

    new commit

--- day22.py
+++ day22.py
@@ -1,5 +1,20 @@
 
 sentence = input("Enter a sentence: ").split(" ")
 values= dict(sorted([x,sentence.count(x)] for x in set(sentence)))
-for k, v in values.items():
-	print(k +":"+ str(v))
+# for k, v in values.items():
+# 	print(k +":"+ str(v))
+
+
+# def word_count(str):
+#     counts = dict()
+#     words = sorted(str.split())
+
+#     for word in words:
+#         if word in counts:
+#             counts[word] += 1
+#         else:
+#             counts[word] = 1
+
+#     return counts
+# you = str(input("Enter a string: "))
+# print(word_count(you))
