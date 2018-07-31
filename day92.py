023ca129c77c891750cafe9926f331aa39db9c3e
Tunde Osborne <osbornetunde@gmail.com>
2018-07-31 16:54:54 +0100

    final solutions

---
+++ day92.py
@@ -0,0 +1,15 @@
+def solve(numheads, numlegs):
+	ns = 'No solution'
+	for i in range(numheads+1):
+		j = numheads - i
+		if 2*i+4*j==numlegs:
+			return i,j
+	return ns,ns
+
+
+numheads = 35
+numlegs = 94
+
+solutions = solve(numheads,numlegs)
+
+print(solutions)
