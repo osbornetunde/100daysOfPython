c63d8be4528aa3149c31d2213ca539cdca214a7b
Tunde Osborne <osbornetunde@gmail.com>
2018-08-08 17:53:05 +0100

    Addition of Question

--- day92.py
+++ day92.py
@@ -1,3 +1,6 @@
+# Write a program to solve a classic ancient Chinese puzzle: 
+# We count 35 heads and 94 legs among the chickens and rabbits in a farm. How many rabbits and how many chickens do we have?
+
 def solve(numheads, numlegs):
 	ns = 'No solution'
 	for i in range(numheads+1):
